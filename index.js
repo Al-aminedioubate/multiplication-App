946e1fc9621d578d97163376f5b9f37c227e709a
Lamine DIOUBATE <domanilamine96@gmail.com>
2026-03-01 06:17:08 -0500

    declaration et initialisation de nos variables terminer

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 //Declaration et initialisation de nos variables
 let score = document.getElementById("score");
+let firstNumber = document.getElementById("firstNum");
+let secondeNumber = document.getElementById("secondNum");
+let input = document.getElementById("answerInput");
+let btn = document.getElementById("submit-btn");
