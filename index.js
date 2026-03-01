bb168ca7ab2ae56e45970c50f7f73145b816bf0a
Lamine DIOUBATE <domanilamine96@gmail.com>
2026-03-01 06:28:44 -0500

    generation des valeurs et leur calcul terminer

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ let firstNumber = document.getElementById("firstNum");
 let secondeNumber = document.getElementById("secondNum");
 let input = document.getElementById("answerInput");
 let btn = document.getElementById("submit-btn");
+
+//la fonction permettant de faire la multiplication
+function multiplication() {
+	let numberOne = Math.floor(Math.random() * 9 + 1);
+	let numberTwo = Math.floor(Math.random() * 9);
+
+	let result = numberOne * numberTwo;
+
+	console.log(numberOne);
+	console.log(numberTwo);
+	console.log("le resultat du calcul est : " + result);
+}
+
+multiplication();
