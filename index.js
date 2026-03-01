646c9ee336cba9b7946f6d4a2a727fbe8c1b3d33
Lamine DIOUBATE <domanilamine96@gmail.com>
2026-03-01 14:19:50 -0500

    reajustement du code

--- index.js
+++ index.js
@@ -5,20 +5,21 @@ let secondeNumber = document.getElementById("secondNum");
 let input = document.getElementById("answerInput");
 let btn = document.getElementById("submit-btn");
 
+//Generateur de nos valeur a multiplier
+let numberOne = Math.floor(Math.random() * 9 + 1);
+let numberTwo = Math.floor(Math.random() * 9);
+
+//affichons les valeurs sur la page
+firstNumber.textContent = numberOne;
+secondeNumber.textContent = numberTwo;
+
 //la fonction permettant de faire la multiplication
 function multiplication() {
-	let numberOne = Math.floor(Math.random() * 9 + 1);
-	let numberTwo = Math.floor(Math.random() * 9);
-
 	let result = numberOne * numberTwo;
 
-	//affichons les valeurs sur la page
-	firstNumber.textContent = numberOne;
-	secondeNumber.textContent = numberTwo;
-
-	console.log(numberOne);
-	console.log(numberTwo);
-	console.log("le resultat du calcul est : " + result);
+	if (input.value == result) {
+		console.log("bravo vous avez trouver la reponse");
+	}
 }
 
 //Evenement du gestion du calcul
