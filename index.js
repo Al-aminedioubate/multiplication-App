723a4bc0504b79b7fd3ab02f35573e07462fc51d
Lamine DIOUBATE <domanilamine96@gmail.com>
2026-03-02 05:48:10 -0500

    ajustement du code

--- index.js
+++ index.js
@@ -7,32 +7,34 @@ let btn = document.getElementById("submit-btn");
 
 //Generateur de nos valeur a multiplier
 let numberOne = Math.floor(Math.random() * 9 + 1);
-let numberTwo = Math.floor(Math.random() * 9);
+let numberTwo = Math.floor(Math.random() * 9) + 1;
 
 //affichons les valeurs sur la page
 firstNumber.textContent = numberOne;
 secondeNumber.textContent = numberTwo;
 
+//Le score sauvegarder s'affichera directement dans le score lors du rafraichiment de la page
+let savedScore = parseInt(localStorage.getItem("score")) || 0;
+score.textContent = `score : ${savedScore}`;
+
 //la fonction permettant de faire la multiplication
 function multiplication() {
 	let result = numberOne * numberTwo;
+	let user = parseInt(input.value);
 
 	// Récupérons le score existant
-	if (input.value === "") return;
+	if (user == "") return;
 
 	let scoreCount = parseInt(localStorage.getItem("score")) || 0;
 
-	if (input.value === result) {
+	if (user == result) {
 		scoreCount++;
 	} else {
-		scoreCount--;
+		scoreCount = Math.max(0, scoreCount - 1);
 	}
-
-	//console.log("votre score eest " + scoreCount);
 
 	//Sauvegardons le nouveau score ici
 	localStorage.setItem("score", scoreCount);
-
 	score.textContent = `score : ${scoreCount}`;
 }
 
